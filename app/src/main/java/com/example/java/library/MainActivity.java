63cc08a1391008bd691dc047a646d96049662c1f
valentindp <valentindp.post@gmail.com>
2016-12-14 20:49:12 +0200

    Scroll

--- app/src/main/java/com/example/java/library/MainActivity.java
+++ app/src/main/java/com/example/java/library/MainActivity.java
@@ -2,18 +2,27 @@ package com.example.java.library;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 
+import com.example.library.view.SwipeItemView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private SwipeItemView mView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mView = (SwipeItemView) findViewById(R.id.swipe_item);
 
     }
 
-
-
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        mView.dispatchTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
 }
--- library/src/main/java/com/example/library/view/SwipeItemView.java
+++ library/src/main/java/com/example/library/view/SwipeItemView.java
@@ -1,25 +1,55 @@
 package com.example.library.view;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
+import android.support.v4.view.GestureDetectorCompat;
+import android.support.v4.view.ViewCompat;
+import android.text.Layout;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.View;
 import android.widget.FrameLayout;
+
 
 /**
  * Created by java on 14.12.2016.
  */
 
-public class SwipeItemView extends FrameLayout {
+public class SwipeItemView extends FrameLayout implements GestureDetector.OnGestureListener {
+
+    private GestureDetectorCompat mGestureDetectorCompat;
+    private View mTopView;
+    private View mBottomView;
+    private boolean isLeftDirection;
 
     public SwipeItemView(Context context) {
         super(context);
+        mGestureDetectorCompat = new GestureDetectorCompat(getContext(), this);
     }
 
     public SwipeItemView(Context context, AttributeSet attrs) {
         super(context, attrs);
+
+        mBottomView = new View(getContext());
+        mTopView = new View(getContext());
+
+        LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+
+        mTopView.setLayoutParams(params);
+        mBottomView.setLayoutParams(params);
+
+        ViewCompat.setBackground(mTopView, new ColorDrawable(Color.RED));
+        ViewCompat.setBackground(mBottomView, new ColorDrawable(Color.GREEN));
+
+        addView(mBottomView);
+        addView(mTopView);
+
+        mGestureDetectorCompat = new GestureDetectorCompat(getContext(), this);
     }
 
     public SwipeItemView(Context context, AttributeSet attrs, int defStyleAttr) {
@@ -44,20 +74,61 @@ public class SwipeItemView extends FrameLayout {
 
     @Override
     public boolean dispatchTouchEvent(MotionEvent ev) {
-        switch (ev.getAction()) {
-            case MotionEvent.ACTION_DOWN: {
-                Log.d(SwipeItemView.class.getSimpleName(), "down");
-                break;
-            }
-            case MotionEvent.ACTION_MOVE: {
-                Log.d(SwipeItemView.class.getSimpleName(), "move");
-                break;
-            }
-            case MotionEvent.ACTION_UP: {
-                Log.d(SwipeItemView.class.getSimpleName(), "up");
-                break;
+        mGestureDetectorCompat.onTouchEvent(ev);
+
+        if (ev.getAction() == MotionEvent.ACTION_UP) {
+            if (isLeftDirection) {
+                goToLeft();
+            } else {
+                goToRight();
             }
         }
         return super.dispatchTouchEvent(ev);
+    }
+
+    private void goToLeft() {
+        mTopView.animate().x(0).setDuration(300).start();
+    }
+
+    private void goToRight() {
+        mTopView.animate().x(mTopView.getMeasuredWidth() / 2).setDuration(300).start();
+    }
+
+
+    @Override
+    public boolean onDown(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        Log.d(SwipeItemView.class.getSimpleName(), "onScroll");
+
+        if (motionEvent1.getX() < (mTopView.getMeasuredWidth() / 2)) {
+            mTopView.setX(motionEvent1.getX());
+        }
+        isLeftDirection = motionEvent1.getX() - motionEvent.getX() < 0;
+
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        return false;
     }
 }
